2f84a47b85adb464a5c51fd8ba51975af5105d31
JoaoNun <joaopnunes252@gmail.com>
2025-06-07 10:21:40 -0300

    testes unitários product

---
+++ product/tests/test_category.py
@@ -0,0 +1,14 @@
+import pytest
+from product.models import Category
+
+@pytest.mark.django_db
+def create_create_category():
+    category = Category.objects.create(
+        title="test_title",
+        slug="test_slug",
+        description="test_description"
+    )
+
+    assert category.title == "test_title"
+    assert category.slug == "test_slug"
+    assert category.description == "test_description"
